9d0ce65fc81a76c8696c4499d068fb6a8f1de726
FurySwordXD <sainathganesh1@gmail.com>
2019-12-05 13:12:14 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Main
 {
-	public static void main(String args[])
+	public static void emp()
 	{
-		//System.out.println("Args: " + args[1]);
 		Scanner scanner = new Scanner(System.in);
-		//System.out.println(scanner.next());
+
 		System.out.print("Enter ID: ");
 		int id = scanner.nextInt();
 		System.out.print("Enter Name: ");
@@ -15,7 +14,27 @@ public class Main
 		double basic = scanner.nextDouble();
 
 		Employee e = new Employee(id, name, basic);
-		
 		e.ShowSalary();
+
+		scanner.close();
+	}
+
+	public static void PlanetAge()
+	{
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter age: ");
+		double age = scanner.nextDouble();
+		//System.out.print("Enter number of days for the planet to revolve: ");
+		//double days = scanner.nextDouble();
+
+		PlanetAges p = new PlanetAges(age);
+		p.AllPlanetAges();
+		
+		scanner.close();
+	}
+	public static void main(String args[])
+	{
+		PlanetAge();
 	}
 }
