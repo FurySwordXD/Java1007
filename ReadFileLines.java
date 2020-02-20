2c6bb5a3db3e3b602dfee42b14084356079cfe05
FurySwordXD <sainathganesh1@gmail.com>
2020-02-20 13:15:50 +0530

    Create ReadFileLines.java

---
+++ ReadFileLines.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.nio.*;
+import java.nio.file.Path;
+import java.io.FileReader;
+
+class ReadFileLines
+{
+    public static int findOccurences(String data, String find)
+    {
+        int count = 0, fromIndex = 0;
+        
+        while ((fromIndex = data.indexOf(find, fromIndex)) != -1 ){
+ 
+            //System.out.println("Found at index: " + fromIndex);
+            count++;
+            fromIndex++;
+            
+        }
+        return count + 1;
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        //int n = scanner.nextInt();
+        //scanner.nextLine();
+        while(true)
+        {
+            String path = scanner.nextLine();
+            if(path == "")
+                break;
+            //scanner.nextLine();
+            try
+            {   
+                FileReader file = new FileReader(path);
+                String data = "";
+                int ch;
+                while((ch = file.read()) != -1)  
+                    data += (char)ch;
+                file.close();
+                System.out.println(data);
+                System.out.println(ReadFileLines.findOccurences(data, "\n"));
+            }
+            catch(Exception e)
+            {}
+        }
+
+        scanner.close();
+    }
+}
